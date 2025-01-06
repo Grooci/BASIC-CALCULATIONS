f72934872de44e06dd67fd5d8191bee994bdddd6
Grooci <dannyhumble361@gmail.com>
2025-01-06 11:57:37 +0300

    commit two
    Question three(line 21-32)
    finding net salary of an employee. Declared the gross salary and tax rate. Also declared the net salary and printed.
    Question four(line 37-46)
    finding total revenue. Declared the price1, quantity1, price2, quantity2, price3, quantity3. Also declared the total revenue and its equation and printed.

--- Basic Calculations.c
+++ Basic Calculations.c
@@ -9,7 +9,7 @@ printf("The total cost of items is %d", TotalCost);
 return 0;
 }*/
 
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 
 int main(){
@@ -17,6 +17,32 @@ int HourlyWage = 2500;
 int HoursWorked = 16000;
 int MonthlySalary = HourlyWage * HoursWorked;
 printf("The monthly salary of an employee is %d", MonthlySalary);
+return 0;
+}*/
+
+/*#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int GrossSalary = 5000;
+double TaxRate = 0.1;
+double NetSalary = GrossSalary - (TaxRate * GrossSalary);
+printf("The net salary of an employee is %.2lf", NetSalary);
+return 0;
+}*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int Price1 = 3000;
+int Quantity1 = 100;
+int Price2 = 4500;
+int Quantity2 = 150;
+int Price3 = 6000;
+int Quantity3 = 200;
+int TotalRevenue = (Price1 * Quantity1) + (Price2 * Quantity2) + (Price3 * Quantity3);
+printf("The total revenue is %d", TotalRevenue);
 return 0;
 }
 
