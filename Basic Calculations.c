4bf6db80aff0adb6c28ce27066cd8c523eb23380
Grooci <dannyhumble361@gmail.com>
2025-01-06 11:49:27 +0300

    commit one
    Question one
    finding total cost of items. Declared the quantity and unit price. Also declared the total cost and its equation and printed.
    Question two
    finding monthly salary of an employee. Declared the hourly wage and hours worked. Also declared the monthly salary and its equation and printed.

---
+++ Basic Calculations.c
@@ -0,0 +1,24 @@
+/*#include <stdio.h>
+
+
+int main(){
+int Quantity = 2000;
+int UnitPrice = 1500;
+int TotalCost = Quantity * UnitPrice;
+printf("The total cost of items is %d", TotalCost);
+return 0;
+}*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int HourlyWage = 2500;
+int HoursWorked = 16000;
+int MonthlySalary = HourlyWage * HoursWorked;
+printf("The monthly salary of an employee is %d", MonthlySalary);
+return 0;
+}
+
+
+
